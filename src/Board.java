226ce956d71cce6de9f5dc3809800402ff2720c3
BT_1N3_03 <sample@bths.com>
2024-12-03 10:49:53 -0500

    commit3

--- src/Board.java
+++ src/Board.java
@@ -1,14 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is to create the board along with its difficulty
+ *
+ * @author Zachary Lin and Leo Zheng
+ */
 public class Board {
+    /** The grid size; how many tiles each row/column is*/
     private int grid;
-    private String gamemode;
+    /** The alphabet is the y axis of the board*/
     private List<String> alphabet = new ArrayList<String>();
+    /** all possible cords of the board*/
     private List <String> cords = new ArrayList<String>();
+    /** notAllowBombs is the set of cords that a bomb cannot spawn at*/
     private List <String> notAllowBombs = new ArrayList<String>();
 
-
+    /**
+     * Instantiates a board object
+     *
+     * @param gamemode The difficulty of the board
+     */
     public Board(String gamemode) {
         if (gamemode.equalsIgnoreCase(("easy"))) {
             grid = 8;
@@ -21,54 +33,43 @@ public class Board {
         }
     }
 
-    public void setNotAllowBombs(String userInput) {
-        String firstChar = userInput.substring(0, 1);
-        int secondChar = Integer.parseInt(userInput.substring(1));
-        if (firstChar.equals(alphabet.getFirst())) {
-            if (secondChar == 1) {
-                for (int i = 1;i < 5; i++) {
-                    String add = "";
-                    if (i < 3) {
-                        add +=  alphabet.getFirst() + i;
-                    } else {
-                        add += alphabet.get(1) + (i - 2);
-                    }
-                    notAllowBombs.add(add);
-                }
-            } else if (secondChar == grid) {
-                for (int i = grid; i > grid - 4; i--) {
-                    String add = "";
-                    if (i < 3) {
-                        add +=  alphabet.getFirst() + i;
-                    } else {
-                        add += alphabet.get(1) + (i + 2);
-                    }
-                    notAllowBombs.add(add);
-                }
-            } else {
-                int num = secondChar - 1;
-                for (int i = 0; i < 6; i++) {
-                    String add = "";
-                    if (i < 3) {
-                        add = alphabet.getFirst() + (num + i);
-                    } else {
-                        add = alphabet.get(1) + (num + i - 3);
-                    }
-                    notAllowBombs.add(add);
-                }
-            }
-        }
-        else if (firstChar.equals(alphabet.getLast())) {
-            
-        }
-    }
-
+    /**
+     * sequence that creates a board
+     */
     public void createBoard() {
         createAlphabet();
         createCord();
         printBoard();
     }
 
+    /**
+     * Makes an area where the bombs cannot spawn in
+     *
+     * @param userInput the user's input
+     */
+    public void setNotAllowBombs(String userInput) {
+        int firstIdx = alphabet.indexOf(userInput.substring(0, 1)) - 1;
+        int secondChar = Integer.parseInt(userInput.substring(1)) - 1;
+
+        for (int i = 0; i < 9; i++) {
+            String add = "";
+            if (firstIdx >= 0 && secondChar > 0 && i < 3) {
+                add = alphabet.get(firstIdx - 1) + (secondChar + i);
+            } else if (firstIdx >= 0 && secondChar > 0 && i < 6) {
+                add = alphabet.get(firstIdx) + (secondChar + i - 3);
+            } else if (firstIdx >= 0 && secondChar > 0) {
+                add = alphabet.get(firstIdx + 1) + (secondChar + i - 6);
+            }
+            if (!add.isEmpty()) {
+                notAllowBombs.add(add);
+                add = "";
+            }
+        }
+    }
+
+    /**
+     * prints the board so the user can see it
+     */
     private void printBoard() {
         for (int i = 0; i < grid + 1; i++) {
             if (i < grid) {
@@ -100,6 +101,9 @@ public class Board {
         }
     }
 
+    /**
+     * creates each unit of the y-axis of the board
+     */
     private void createAlphabet() {
         for (char i = 65; i <= 65 + grid; i++) {
             String letter = i + "";
@@ -107,6 +111,9 @@ public class Board {
         }
     }
 
+    /**
+     * creates the x-axis, numbers
+     */
     private void createCord() {
         for (int i = 0; i < alphabet.size(); i++) {
             for (int j = 1; j <= grid; j++) {
@@ -114,8 +121,20 @@ public class Board {
             }
         }
     }
+
+    /**
+     * creates bombs
+     */
+    private void createBomb() {
+        int bombsAdded = 0;
+        while (bombsAdded != 100) {
+            int firstIdxChar = (int) (Math.random() * (grid + 1));
+            int secondChar = (int) (Math.random() * (grid + 1));
+            String add = alphabet.get(firstIdxChar) + secondChar;
+            if (!cords.contains(add) && !notAllowBombs.contains(add)) {
+                cords.add(add);
+                bombsAdded++;
+            }
+        }
+    }
 }
-
-
-
-
